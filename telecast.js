7b667733eff75bbc3f4ebeb05b4ff55b5a250250
jasoncaserta <jasonecaserta@gmail.com>
2018-01-16 00:56:13 -0500

    Revert "IM A GOD"
    This reverts commit 436ac977df61b9726e596f62951016b3ec105f16.

--- telecast.js
+++ telecast.js
@@ -1,4 +1,4 @@
-
+// var webdriver = require('selenium-webdriver'), By  = webdriver.By, until = webdriver.until;
 // var chromeCapabilities = webdriver.Capabilities.chrome();
 // var o = new chrome.Options();
 // o.addArguments("Users/jasoncaserta/Library/Application Support/Google/Chrome/Default");
@@ -6,34 +6,43 @@
 // driver.get('https://web.telegram.org/#/login')
 
 
-var webdriver = require('selenium-webdriver'), By  = webdriver.By, until = webdriver.until;
+var webdriver = require('selenium-webdriver');
 const username = require('username');
 var chrome = require('selenium-webdriver/chrome');
 
 username().then(username => {
     var o = new chrome.Options();
-    o.addArguments("user-data-dir=/Users/" + username + "/Library/Application\ Support/Google/Chrome/Default");
-    var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).setChromeOptions(o).build();
+    o.addArguments("user-data-dir=/Users/"+ username +"/Library/Application\ Support/Google/Chrome/Default");
+    var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).setChromeOptions(o).build(); 
     driver.get('https://web.telegram.org/#/login');
 
-    var query = driver.wait(until.elementLocated(By.xpath('//*[@id="ng-app"]/body/div[1]/div[2]/div/div[2]/div[3]/div/div[2]/div[1]')));
-
-    query.then(() => {
-        console.log('found Chat!');
-        // let groupchats = [];
-        // const getGroupChats = driver.findElements(By.className("im_dialog_wrap"));
-        // getGroupChats.then(chats=>{
-        //     console.log('chats:-------\n', chats);
-        // })
-
-        //Check is this works
-        driver.sleep(5000);
-        driver.findElements(By.xpath('//*[@id="ng-app"]/body/div[1]/div[2]/div/div[2]/div[3]/div/div[2]/div[1]/div/div[1]/div[2]/div[2]/div[4]/div[2]/div/div/div[2]/div')).then((elements) => {
-            elements.map((oneElement) => [
-                oneElement.getText().then((text) => {
-                    console.log('this is one message:', text);
-                })
-            ])
-        })
-    })
 });
+
+//	/Users/jasoncaserta/Library/Application Support/Google/Chrome/Default
+
+// var driver = new webdriver.Builder().forBrowser('chrome').build();
+
+// driver.get('https://web.telegram.org/#/login')
+
+// //Element which prompts to select groupchat to start chatting in.
+
+//var query = driver.wait(until.elementLocated(By.css("body > div.page_wrap > div.im_page_wrap.clearfix.im_page_peer_not_selected > div > div.im_history_col_wrap.noselect > div.im_history_not_selected_wrap > div")));
+
+// query.then(()=> {
+//     console.log('found Element!');
+//     // let groupchats = [];
+//     // const getGroupChats = driver.findElements(By.className("im_dialog_wrap"));
+//     // getGroupChats.then(chats=>{
+//     //     console.log('chats:-------\n', chats);
+//     // })
+
+//     //Check is this works
+//     driver.sleep(15000);
+//     driver.findElements(By.xpath('//*[@id="ng-app"]/body/div[1]/div[2]/div/div[2]/div[3]/div/div[2]/div[1]/div/div[1]/div[2]/div[2]/div[4]/div[2]/div/div/div[2]/div')).then((elements)=>{
+//         elements.map((oneElement)=>[
+//             oneElement.getText().then((text)=>{
+//                 console.log('this is one message:', text);
+//             })
+//         ])
+//     })
+// })
