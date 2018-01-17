93a14b13b59383760615852161cd2afa6e6f4e11
Kuba <jmn417@nyu.edu>
2018-01-16 23:44:11 -0500

    incorrect code

--- index.js
+++ index.js
@@ -28,9 +28,56 @@ username().then(username => {
         //Check is this works
         driver.sleep(10000);
         driver.findElements(By.css('li')).then((chats) => {
-            chats.map((singleChat) => {
-                singleChat 
+            // chats.map((singleChat) => {
+            //     console.log(singleChat);
+                
+            // })
+            // console.log(chats);
+            chats[0].findElements(By.css('*')).then(newElements=>{
+                // console.log('this is one new element:', newElements);
+                newElements[0].getContents().then(contents=>{
+                    console.log('contents:', contents);
+                })
+                
+                // newElements[1].findElements(By.css("*")).then(newerElements=>{
+                //     console.log('this is the second layer:', newerElements);
+                // })
+                    // console.log('these are the second children elements:', newerElements);
+                    // newerElements[1].getText().then(messageCount=>{
+                    //     console.log('this is the number:', messageCount);
+                    // })
+                // })
             })
+            
+            
+            
+            // for(let a=0; a< chats.length; a++){
+            //     chats[a].findElements(By.css('*')).then(newElements=>{
+            //         // console.log('this is one new element:', newElements);
+            //         newElements[0].findElements(By.css("*")).then(newerElements=>{
+            //             console.log('these are the second children elements:', newerElements);
+            //             // newerElements[1].getText().then(messageCount=>{
+            //             //     console.log('this is the number:', messageCount);
+            //             // })
+            //         })
+            //     })
+            // }
+            // ___________________________
+            // const domParser = new DOMParser();
+            // const doc = domParser.
+
+
+
+
+
+
+
+
+
+
+
+            // ___________________________
+
         })
         // driver.findElement(By.xpath('//*[@id="ng-app"]/body/div[1]/div[2]/div/div[1]/div[2]/div/div[1]/ul/li[1]')).then((messageCountElement) => {
         //     messageCountElement.map((oneElement) => [
